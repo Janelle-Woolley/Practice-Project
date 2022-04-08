8c7da7bff255808ae604c84be873e340e2de1c67
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-04-08 12:34:02 +1200

    New project with comment plan

---
+++ album_rater.py
@@ -0,0 +1,16 @@
+##
+# album_rater.py
+# allow user to rate albums
+#
+# Author: Janelle Woolley
+# 08/04/22 Version_1
+
+
+# function for adding album
+# function for deleting album
+# function for editing album
+# function for rating album
+# function for printing album
+# Main routine
+# create nested dictionary
+# Menu #Turn into function???
