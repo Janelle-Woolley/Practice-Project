f19b6288302ef59699a9035446b162e2d21168f3
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-04-11 13:40:14 +1200

    Added a main routine with a nested dictionary
    Nested dictionary is for the albums

--- album_rater.py
+++ album_rater.py
@@ -11,6 +11,24 @@
 # function for editing album
 # function for rating album
 # function for printing album
+# function recomending an album
 # Main routine
 # create nested dictionary
 # Menu #Turn into function???
+
+if __name__ == "__main__":
+    albums = {1:{"Title":"Sour",
+                 "Artist":"Olivia Rodrigo",
+                 "Genre":"Pop" "Rating":5},
+              2:{"Title":"Promises",
+                 "Artist":"Sam Shepherd",
+                 "Genre":"Jazz" "Rating":2},
+              3:{"Title":"Sound Ancestors",
+                 "Artist":"Madlib",
+                 "Genre":"Hip-Hop" "Rating":0},
+              4:{"Title":"Valentine",
+                 "Artist":"Snail Mail",
+                 "Genre":"Alternative" "Rating":4},
+              5:{"Title":"Daddy's Home",
+                 "Artist":"St. Vincent",
+                 "Genre":"Pop" "Rating":2}}
