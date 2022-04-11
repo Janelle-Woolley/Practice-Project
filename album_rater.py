0508084302f11be7af772c7234a16784fbee04d6
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-04-12 09:17:50 +1200

    Added function for adding albums and edited menu
    Edited menu function variable names
    Code will now loop back to the menu
    Started a print function

--- album_rater.py
+++ album_rater.py
@@ -21,28 +21,57 @@
 # create nested dictionary
 # Menu function
 
-# Created By Phoebe
-def getting_album():
+# Created By Phoebe, I edited variable names
+def menu():
+    """
+    Ask user to select an option
+    Returns selected option
+    """
     choices = [1, 2, 3, 4, 5, 6]
-    getting_albums = True
-    while getting_albums:
+    getting_option = True
+    while getting_option:
         try:
             choice = int(input("\nPlease chose your option\n"
-                           "1: add album\n"
-                           "2: delete album\n"
-                           "3: edit album\n"
-                           "4: rate album\n"
-                           "5: print albums\n"
-                           "6: recomend albums\n"))
+                               "1: add album\n"
+                               "2: delete album\n"
+                               "3: edit album\n"
+                               "4: rate album\n"
+                               "5: print albums\n"
+                               "6: recomend albums\n"
+                               "0: exit\n"))
             if choice in choices:
                 print("you have chosen {}.\n".format(choice))
-                getting_albums = False
+                getting_option = False
+
         except ValueError:
             print("enter a valid number")
+
         return choice
 
+
 def add_album(dictionary):
-    pass
+    """
+    Asks for user inputs and adds them to dictionary
+    """
+    adding_albums = True
+    while adding_albums:
+        album = input("Please enter the title of the album\n"
+                      "(or type 'q' to quit)\n"
+                      ": ")
+        if album == "q":
+            adding_albums = False
+        else:
+           artist = input("Please enter the artist: ")
+           genre = input("Please enter the genre: ")
+           rating = input("Please give it a rating: ")
+           key = len(dictionary) + 1
+
+           dictionary.update({key:{album, artist, genre, rating}})
+           adding_albums = False
+
+
+def print_albums(dictionary):
+    print(dictionary)
 
 if __name__ == "__main__":
     albums = {1:{"Title":"Sour",
@@ -60,5 +89,14 @@ if __name__ == "__main__":
               5:{"Title":"Daddy's Home",
                  "Artist":"St. Vincent",
                  "Genre":"Pop", "Rating":2}}
-
-    menu_option = getting_album()
+    
+    # loop so meun will keep coming up after completing an option
+    code_running = True
+    while code_running:
+        menu_option = menu()
+        if menu_option == 0:
+            code_running = False
+        elif menu_option == 1:
+            add_album(albums)
+            print_albums(albums)
+        
