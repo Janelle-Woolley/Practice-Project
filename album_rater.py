ce9d7e8dab773bb3050f2b98b285469660bdbda8
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-05-02 13:53:47 +1200

    Added Delete Version 1 from phoebe

--- album_rater.py
+++ album_rater.py
@@ -5,21 +5,22 @@
 # Author: Janelle Woolley, Phoebe and Syuni
 # 08/04/22 Version_1
 
-## TO DO:
+# TO DO:
 # function for deleting album
 # function for editing album
 # function for rating album
-# function for printing album
 # function recomending an album
 # call functions
 
-## DOING:
-# function for adding album
+# DOING:
+# function for printing album
 
-## DONE:
+# DONE:
 # Main routine
 # create nested dictionary
 # Menu function
+# function for adding album
+
 
 # Created By Phoebe, I edited variable names
 def menu():
@@ -61,35 +62,56 @@ def add_album(dictionary):
         if album == "q":
             adding_albums = False
         else:
-           artist = input("Please enter the artist: ")
-           genre = input("Please enter the genre: ")
-           rating = input("Please give it a rating: ")
-           key = len(dictionary) + 1
+            artist = input("Please enter the artist: ")
+            genre = input("Please enter the genre: ")
+            rating = input("Please give it a rating: ")
+            key = len(dictionary) + 1
 
-           dictionary.update({key:{album, artist, genre, rating}})
-           adding_albums = False
+            dictionary.update({key: {"Title": album,
+                                     "Artist": artist,
+                                     "Genre": genre,
+                                     "Rating": rating}})
+            adding_albums = False
+
+
+# Created By Phoebe, I edited variable names to work with my code
+def delete_album(dictionary):
+    deleting_albums = True
+    while deleting_albums:
+        print(dictionary)
+        delete = int(input("\nPlease select the album number you would "
+                           "like to delete, or press 'q' to quit: "))
+        if delete == "q":
+                deleting_albums = False
+        else:
+            print("You have chose to delete album {}.".format(delete))
+            del dictionary[delete]
+            key = len(dictionary) - 1
+            dictionary.update()
+            deleting_albums = False
 
 
 def print_albums(dictionary):
     print(dictionary)
 
+
 if __name__ == "__main__":
-    albums = {1:{"Title":"Sour",
-                 "Artist":"Olivia Rodrigo",
-                 "Genre":"Pop", "Rating":5},
-              2:{"Title":"Promises",
-                 "Artist":"Sam Shepherd",
-                 "Genre":"Jazz", "Rating":2},
-              3:{"Title":"Sound Ancestors",
-                 "Artist":"Madlib",
-                 "Genre":"Hip-Hop", "Rating":0},
-              4:{"Title":"Valentine",
-                 "Artist":"Snail Mail",
-                 "Genre":"Alternative", "Rating":4},
-              5:{"Title":"Daddy's Home",
-                 "Artist":"St. Vincent",
-                 "Genre":"Pop", "Rating":2}}
-    
+    albums = {1: {"Title": "Sour",
+                  "Artist": "Olivia Rodrigo",
+                  "Genre": "Pop", "Rating": 5},
+              2: {"Title": "Promises",
+                  "Artist": "Sam Shepherd",
+                  "Genre": "Jazz", "Rating": 2},
+              3: {"Title": "Sound Ancestors",
+                  "Artist": "Madlib",
+                  "Genre": "Hip-Hop", "Rating": 0},
+              4: {"Title": "Valentine",
+                  "Artist": "Snail Mail",
+                  "Genre": "Alternative", "Rating": 4},
+              5: {"Title": "Daddy's Home",
+                  "Artist": "St. Vincent",
+                  "Genre": "Pop", "Rating": 2}}
+
     # loop so meun will keep coming up after completing an option
     code_running = True
     while code_running:
@@ -99,4 +121,6 @@ if __name__ == "__main__":
         elif menu_option == 1:
             add_album(albums)
             print_albums(albums)
-        
+        elif menu_option == 2:
+            delete_album(albums)
+            print_albums(albums)
