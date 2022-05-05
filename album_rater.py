5d4e0a9ecbfb20942586dffc26e28bcae415dc2b
Janelle-Woolley <103225532+Janelle-Woolley@users.noreply.github.com>
2022-05-05 14:43:56 +1200

    Started Rating and print function
    Rating - Still needs work, should split into two function
    Print - Is broken, will fix it later

--- album_rater.py
+++ album_rater.py
@@ -2,17 +2,16 @@
 # album_rater.py
 # allow user to rate albums
 #
-# Author: Janelle Woolley, Phoebe and Syuni
+# Author: Janelle Woolley, Phoebe
 # 08/04/22 Version_1
 
 # TO DO:
-# function for deleting album
 # function for editing album
-# function for rating album
 # function recomending an album
 # call functions
 
 # DOING:
+# function for rating album
 # function for printing album
 
 # DONE:
@@ -20,6 +19,7 @@
 # create nested dictionary
 # Menu function
 # function for adding album
+# function for deleting album
 
 
 # Created By Phoebe, I edited variable names
@@ -41,7 +41,7 @@ def menu():
                                "6: recomend albums\n"
                                "0: exit\n"))
             if choice in choices:
-                print("you have chosen {}.\n".format(choice))
+                print("You have chosen {}.\n".format(choice))
                 getting_option = False
 
         except ValueError:
@@ -74,13 +74,13 @@ def add_album(dictionary):
             adding_albums = False
 
 
-# Created By Phoebe, I edited variable names to work with my code
+# Created By Phoebe, I edited code to fit
 def delete_album(dictionary):
     deleting_albums = True
     while deleting_albums:
-        print(dictionary)
-        delete = int(input("\nPlease select the album number you would "
-                           "like to delete, or press 'q' to quit: "))
+        delete = input("\nPlease select the album number you would "
+                       "like to delete, or press 'q' to quit: ")
+        
         if delete == "q":
                 deleting_albums = False
         else:
@@ -91,9 +91,56 @@ def delete_album(dictionary):
             deleting_albums = False
 
 
-def print_albums(dictionary):
-    print(dictionary)
+def rate_album(dictionary):
+    album_rating_change = input("\nPlease select the key number "
+                                "for the album you would like to "
+                                "change the rating for"
+                                "\nOR press 'q' to quit: "))
+    
+    album_info = dictionary[album_rating_change]
+    album = album_info["Title"]
+    artist = album_info["Artist"]
+    genre = album_info["Genre"]
+    rating = album_info["Rating"]
+    key = album_rating_change
 
+    getting_rating = True
+    while getting_rating:
+        try:
+            new_rating = int(input("Please give the album a rating out of 5: "))
+
+            if new_rating < 0 or new_rating > 5:
+                print("Please rate the album from 0 to 5")
+
+            elif new_rating == rating:
+                print("The album already has this rating "
+                      "please change it")
+
+            elif new_rating != rating and new_rating >= 0 and new_rating <= 5:
+                dictionary.update({key: {"Title": album,
+                                     "Artist": artist,
+                                     "Genre": genre,
+                                     "Rating": new_rating}})
+                getting_rating = False
+
+            else:
+                print("Please enter a number between 0 and 5")
+                
+        except ValueError:
+            print("Please enter a whole number")        
+
+
+def print_albums(dictionary):
+    for key in dictionary.keys():
+        album = key["Title"]
+        artist = key["Artist"]
+        genre = key["Genre"]
+        rating = key["Rating"]
+
+        print("{}\t{}\t{}\t{}\t{}".format(key, album, artist, genre, rating))
+
+def prints(dictionary):
+    print(dictionary)
 
 if __name__ == "__main__":
     albums = {1: {"Title": "Sour",
@@ -122,5 +169,13 @@ if __name__ == "__main__":
             add_album(albums)
             print_albums(albums)
         elif menu_option == 2:
+            print_albums(albums)
             delete_album(albums)
             print_albums(albums)
+        elif menu_option == 4:
+            prints(albums)
+            rate_album(albums)
+        elif menu_option == 5:
+            print_albums(albums)
+        else:
+            print("Please enter a valid option")
